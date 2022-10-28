d3b77927b3ff0c6ad8ed945ce9c422c215515db7
reubenJcorriea <reuben.corriea@gmail.com>
2022-10-28 11:58:26 -0400

    Toolbar created

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,19 @@ import MSPlacesPages from "../MSPlacesPage/MSPlacesPage";
 import { useState } from "react";
 
 function HomePage() {
-    return <div className="homepage"></div>;
+  return (
+    <>
+      <div className="homepage">
+
+        <div className="nav">
+            <div className="nav-container">
+
+            </div>
+        </div>
+      
+      </div>;
+    </>
+  );
 }
 
 export default HomePage;
