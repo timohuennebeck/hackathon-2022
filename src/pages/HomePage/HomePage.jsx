d2c7b89840e94ae8fc59aa8e8f46479662cc869a
reubenJcorriea <reuben.corriea@gmail.com>
2022-10-28 12:38:35 -0400

    Hompeage added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,47 @@
 import "./HomePage.scss";
-import MSPlacesPages from "../MSPlacesPage/MSPlacesPage";
-import { useState } from "react";
+
+import timerIcon from "../../assets/images/317-box.png";
+import videoIcon from "../../assets/images/Camera off.png";
+import micIcon from "../../assets/images";
+
+
 
 function HomePage() {
   return (
     <>
       <div className="homepage">
-
         <div className="nav">
-            <div className="nav-container">
+          <div className="nav-container">
+
+            <div>
+              <img className="nav-container__timer" src={timerIcon} alt="timer"></img>
+            </div>
+            <div>
+            <img className="nav-container__video" src={videoIcon} alt="timer"></img>
+            </div>
+            <div>
 
             </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+
+          </div>
         </div>
-      
-      </div>;
+      </div>
+      ;
     </>
   );
 }
